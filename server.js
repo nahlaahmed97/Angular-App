bba4bb1235d409e04f249535b1fe01338630bf5a
nahlaahmed97 <nahla.elaskry97@gmail.com>
2020-08-11 16:22:46 +0200

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname+ '/dist/angular-lab'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+
+        'dist/angular-lab/index.html'))
+});
+app.listen(process.env.PORT || 8080);
